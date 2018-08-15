0133aa9ad83ac70396539ff40a2cb97e1a4fc4fe
Crystal Espaillat <crystalespaillat@Crystals-MBP.home>
2018-08-15 14:46:41 -0400

    Added dummy data. Fully functional. Missing the matching logic.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,131 @@
-// ===============================================================================
 // DATA
-// Below data will hold all of the available fit friends.
-// ===============================================================================
+// /////////////////////////////////////////////////////////////////////////////
 
 var friends = [
-    {
-      name: "Crystal",
-      photo: "crystal@example.com",
-      scores: [
-        
-      ]
-    }
+  {
+    name: "Alex",
+    photo: "http://i68.tinypic.com/259a2s0.jpg",
+    scores: [
+      "3",
+      "4",
+      "5",
+      "1",
+      "5",
+      "5",
+      "4",
+      "1",
+      "5",
+      "5" ]
+  },
+  {
+    name: "Bobby",
+    photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1" ]
+  },
+  {
+    name: "Crystal",
+    photo: "http://i63.tinypic.com/x55fuh.jpg",
+    scores: [
+      "5",
+      "4",
+      "5",
+      "4",
+      "5",
+      "1",
+      "1",
+      "5",
+      "4",
+      "5" ]
+  },
+  {
+    name: "Debby",
+    photo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2" ]
+  },
+  {
+    name: "Erica",
+    photo: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3" ]
+  },
+  {
+    name: "Frank",
+    photo: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4" ]
+  },
+  {
+    name: "George",
+    photo: "https://images.pexels.com/photos/326559/pexels-photo-326559.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5" ]
+  },
+
+  {
+    name: "Helen",
+    photo: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [
+      "5",
+      "1",
+      "5",
+      "1",
+      "5",
+      "1",
+      "5",
+      "1",
+      "5",
+      "1" ]
+  }
+
   ];
-  
-  // Note how we export the array. This makes it accessible to other files using require.
+
+  // Exporting the friends array.
   module.exports = friends;
   
